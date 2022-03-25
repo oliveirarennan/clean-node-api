c7105253d0d5139bafa95219fd722dbae9e13496
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-03-25 12:43:33 -0300

    test: ensure SignUpController call EmailValidador with correct email

--- src/presentation/controllers/SignupController.spec.ts
+++ src/presentation/controllers/SignupController.spec.ts
@@ -98,4 +98,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
+
+  it('Should call EmailValidador with correct email', () => {
+    const { sut, emailValidatorStub } = makeSut()
+
+    const isValidSpy = jest.spyOn(emailValidatorStub, 'isValid')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        password_confirmation: 'any_password'
+      }
+    }
+    sut.handle(httpRequest)
+    expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
+  })
 })
