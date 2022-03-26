8d650682ef39e6baa0f4fd18ac31f9cb185ee71d
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-03-26 10:11:35 -0300

    feat: ensure SignUpController return 400 if password confirmation fails

--- src/presentation/controllers/SignupController.ts
+++ src/presentation/controllers/SignupController.ts
@@ -7,12 +7,17 @@ export class SignUpController implements Controller {
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try {
-      const requiredFields = ['name', 'email', 'password', 'password_confirmation']
+      const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
+
+      if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+
       const isValid = this.emailValidador.isValid(httpRequest.body.email)
 
       if (!isValid) {
