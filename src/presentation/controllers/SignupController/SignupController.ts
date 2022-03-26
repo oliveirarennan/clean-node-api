fdc8d7dd0c6125517fb440248a362bd360b43de7
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-03-26 11:02:24 -0300

    feat: ensure SignUpController return 200 if valid data is provided

--- src/presentation/controllers/SignupController/SignupController.ts
+++ src/presentation/controllers/SignupController/SignupController.ts
@@ -30,7 +30,12 @@ export class SignUpController implements Controller {
         return badRequest(new InvalidParamError('email'))
       }
 
-      this.addAccount.execute({ name, email, password })
+      const account = this.addAccount.execute({ name, email, password })
+
+      return {
+        statusCode: 200,
+        body: account
+      }
     } catch (error) {
       return serverError()
     }
