f2fcc6cfa1ae93f3202baa260313eebe61485c94
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-03-29 12:19:11 -0300

    feat: ensure DBAddAccount calls AddAccountReposioty with correct values

--- src/data/usecases/AddAccount/DbAddAccount.ts
+++ src/data/usecases/AddAccount/DbAddAccount.ts
@@ -1,9 +1,14 @@
-import { AccountModel, AddAccount, AddAccountModel, Encrypter } from './DbAddAccountInterfaces'
+import { AccountModel, AddAccount, AddAccountModel, Encrypter, AddAccountRepository } from './DbAddAccountInterfaces'
 
 export class DbAddAccount implements AddAccount {
-  constructor (private readonly encrypter: Encrypter) {}
-  async execute (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository
+  ) {}
+
+  async execute (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    await this.addAccountRepository.add(Object.assign({}, accountData, { password: hashedPassword }))
     return await new Promise(resolve => resolve(null))
   }
 }
--- src/data/usecases/AddAccount/DbAddAccountInterfaces.ts
+++ src/data/usecases/AddAccount/DbAddAccountInterfaces.ts
@@ -1,3 +1,4 @@
 export * from '../../../domain/models/Account'
 export * from '../../../domain/usecases/AddAccount'
 export * from '../../interfaces/Encrypter'
+export * from '../../interfaces/AddAccountRepository'
