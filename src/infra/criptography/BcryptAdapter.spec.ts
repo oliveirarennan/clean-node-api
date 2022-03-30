145179b7639ec99301e985e092e7360c0eab09a8
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-03-30 15:13:59 -0300

    test: ensure BcryptAdapter throw if bcrypt throws

--- src/infra/criptography/BcryptAdapter.spec.ts
+++ src/infra/criptography/BcryptAdapter.spec.ts
@@ -27,4 +27,12 @@ describe('Bcrypt Adapter', () => {
 
     expect(hash).toBe('hashed_value')
   })
+
+  it('Should throw if bcrypt throws', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'hash').mockImplementationOnce(() => { throw new Error() })
+    const promise = sut.encrypt('any_value')
+
+    await expect(promise).rejects.toThrow()
+  })
 })
