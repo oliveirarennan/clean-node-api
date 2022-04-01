410662296a80df8d66ae6013573e9736ba76ef02
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-01 16:59:08 -0300

    feat: ensure cors is enable

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,6 +1,8 @@
 import { Express } from 'express'
 import { bodyParser } from '../middlewares/BodyParser'
+import { cors } from '../middlewares/Cors'
 
 export default (app: Express): void => {
+  app.use(cors)
   app.use(bodyParser)
 }
