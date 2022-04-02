5174b09d45b648cab8fdbd968563feaaa9e35570
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-02 12:14:15 -0300

    refactor: change account repository folder to AccountRepository

--- src/main/factories/Signup.ts
+++ src/main/factories/Signup.ts
@@ -1,6 +1,6 @@
 import { DbAddAccount } from '../../data/usecases/AddAccount/DbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
-import { AccountMongoRepository } from '../../infra/db/mongodb/account/AccountRepository'
+import { AccountMongoRepository } from '../../infra/db/mongodb/AccountRepository/AccountRepository'
 import { LogMongoRepository } from '../../infra/db/mongodb/LogRepository/LogRepository'
 import { SignUpController } from '../../presentation/controllers/SignupController/SignupController'
 import { Controller } from '../../presentation/interfaces'
