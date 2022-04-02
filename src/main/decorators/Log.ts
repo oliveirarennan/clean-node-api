0c76c2b8e73fdb3982e5a2eafdcb22b1e41ddad5
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-02 11:56:35 -0300

    feat: add LogMongoRepository to SignUpFactory

--- src/main/decorators/Log.ts
+++ src/main/decorators/Log.ts
@@ -1,12 +1,13 @@
+import { LogErrorRepository } from '../../data/interfaces/LogErrorRepository'
 import { Controller, HttpRequest, HttpResponse } from '../../presentation/interfaces'
 
 export class LogControllerDecorator implements Controller {
-  constructor (private readonly controller: Controller) {}
+  constructor (private readonly controller: Controller, private readonly logError: LogErrorRepository) {}
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
 
     if (httpResponse.statusCode === 500) {
-      console.log('decorado')
+      await this.logError.logError(httpResponse.body.stack)
     }
 
     return httpResponse
--- src/main/factories/Signup.ts
+++ src/main/factories/Signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/AddAccount/DbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account/AccountRepository'
+import { LogMongoRepository } from '../../infra/db/mongodb/LogRepository/LogRepository'
 import { SignUpController } from '../../presentation/controllers/SignupController/SignupController'
 import { Controller } from '../../presentation/interfaces'
 import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
@@ -14,6 +15,7 @@ export const makeSignUpController = (): Controller => {
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
   const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const logMongoRepository = new LogMongoRepository()
 
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }
