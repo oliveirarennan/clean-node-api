638ec7521463dd32c40dd5c0d4b70a257d7ae826
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-02 09:24:16 -0300

    feat: ensure LogControllerDeocrator calls controller with correct value

---
+++ src/main/decorators/Log.ts
@@ -0,0 +1,14 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/interfaces'
+
+export class LogControllerDecorator implements Controller {
+  constructor (private readonly controller: Controller) {}
+  async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+    const httpResponse = await this.controller.handle(httpRequest)
+
+    if (httpResponse.statusCode === 500) {
+      console.log('decorado')
+    }
+
+    return httpResponse
+  }
+}
--- src/main/factories/Signup.ts
+++ src/main/factories/Signup.ts
@@ -2,14 +2,18 @@ import { DbAddAccount } from '../../data/usecases/AddAccount/DbAddAccount'
 import { BcryptAdapter } from '../../infra/criptography/BcryptAdapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account/AccountRepository'
 import { SignUpController } from '../../presentation/controllers/SignupController/SignupController'
+import { Controller } from '../../presentation/interfaces'
 import { EmailValidatorAdapter } from '../../utils/EmailValidatorAdapter'
+import { LogControllerDecorator } from '../decorators/Log'
 
-export const makeSignUpController = (): SignUpController => {
+export const makeSignUpController = (): Controller => {
   const salt = 12
   const emailValidatorAdapter = new EmailValidatorAdapter()
   const bcryptAdapter = new BcryptAdapter(salt)
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(bcryptAdapter, accountMongoRepository)
 
-  return new SignUpController(emailValidatorAdapter, dbAddAccount)
+  const signUpController = new SignUpController(emailValidatorAdapter, dbAddAccount)
+
+  return new LogControllerDecorator(signUpController)
 }
