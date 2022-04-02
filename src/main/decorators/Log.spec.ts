d6722ee0f5b251dd92c2f28023b975e6c9da2dcd
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-02 09:34:33 -0300

    refactor: move sut creation to a factory method

---
+++ src/main/decorators/Log.spec.ts
@@ -0,0 +1,54 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/interfaces'
+import { LogControllerDecorator } from './Log'
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+}
+
+const makeControllerStub = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse = {
+        statusCode: 200,
+        body: {
+          name: 'Rennan'
+        }
+      }
+      return await new Promise(resolve => resolve(httpResponse))
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeControllerStub()
+
+  const sut = new LogControllerDecorator(controllerStub)
+
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogControllerDecorator', () => {
+  it('Should call controller handle ', async () => {
+    const { sut, controllerStub } = makeSut()
+
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+})
