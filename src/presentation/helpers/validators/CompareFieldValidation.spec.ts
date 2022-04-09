0dfd5713fda00142b29e6e336a7f19a491ed2dec
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-09 10:10:13 -0300

    test: add tests to CompareFieldsValidation

---
+++ src/presentation/helpers/validators/CompareFieldValidation.spec.ts
@@ -0,0 +1,23 @@
+import { InvalidParamError } from '../../errors'
+import { CompareFieldsValidation } from './CompareFieldsValidation'
+
+const makeSut = (): CompareFieldsValidation => {
+  return new CompareFieldsValidation('field', 'fieldToCompare')
+}
+
+describe('CompareFieldValidation', () => {
+  it('Should Return a InvalidParamError if validation fails', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'wrong_value' })
+
+    expect(error).toEqual(new InvalidParamError('fieldToCompare'))
+  })
+
+  it('Should not Return if validation succeeds', () => {
+    const sut = makeSut()
+
+    const error = sut.validate({ field: 'any_value', fieldToCompare: 'any_value' })
+    expect(error).toBeFalsy()
+  })
+})
