f16ec0ce0e7b9d0347a5ec1845bc2ebb96efcd17
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-09 08:20:00 -0300

    refactor: move password comparation to the ValidationComposite

--- src/main/factories/SignupValidation.spec.ts
+++ src/main/factories/SignupValidation.spec.ts
@@ -1,3 +1,4 @@
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/CompareFieldsValidation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/RequiredFieldValidation'
 import { ValidationComposite } from '../../presentation/helpers/validators/ValidationComposite'
 import { Validation } from '../../presentation/interfaces'
@@ -5,15 +6,17 @@ import { makeSignUpValidation } from './SignupValidation'
 
 jest.mock('../../presentation/helpers/validators/ValidationComposite')
 
-const validations: Validation[] = []
-
-for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
-  validations.push(new RequiredFieldValidation(field))
-}
-
 describe('SignUpValidation Factory', () => {
   it('Should call ValidationComposite with all validations', () => {
     makeSignUpValidation()
+
+    const validations: Validation[] = []
+
+    for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
+      validations.push(new RequiredFieldValidation(field))
+    }
+
+    validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
 
     expect(ValidationComposite).toBeCalledWith(validations)
   })
--- src/main/factories/SignupValidation.ts
+++ src/main/factories/SignupValidation.ts
@@ -1,4 +1,5 @@
 
+import { CompareFieldsValidation } from '../../presentation/helpers/validators/CompareFieldsValidation'
 import { RequiredFieldValidation } from '../../presentation/helpers/validators/RequiredFieldValidation'
 import { ValidationComposite } from '../../presentation/helpers/validators/ValidationComposite'
 import { Validation } from '../../presentation/interfaces'
@@ -9,6 +10,8 @@ export const makeSignUpValidation = (): ValidationComposite => {
   for (const field of ['name', 'email', 'password', 'passwordConfirmation']) {
     validations.push(new RequiredFieldValidation(field))
   }
+
+  validations.push(new CompareFieldsValidation('password', 'passwordConfirmation'))
 
   return new ValidationComposite(validations)
 }
