5243e870ce0770201ec445cbd810e1647438fced
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-13 18:46:13 -0300

    feat: ensure AccountMongoRepository returns an account on loadByEmailSuccess

--- src/infra/db/mongodb/AccountRepository/AccountRepository.ts
+++ src/infra/db/mongodb/AccountRepository/AccountRepository.ts
@@ -1,14 +1,22 @@
 import { AddAccountRepository } from '../../../../data/interfaces/db/AddAccountRepository'
+import { LoadAccountByEmailRepository } from '../../../../data/interfaces/db/LoadAccountByEmailRepository'
 import { AccountModel } from '../../../../domain/models/Account'
 import { AddAccountModel } from '../../../../domain/usecases/AddAccount'
 import { MongoHelper } from '../helpers/MongoHelper'
 
-export class AccountMongoRepository implements AddAccountRepository {
+export class AccountMongoRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = await MongoHelper.getCollection('accounts')
     const result = await accountCollection.insertOne(accountData)
     const account = result.ops[0]
 
     return MongoHelper.map(account)
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts')
+    const account = await accountCollection.findOne({ email })
+
+    return account && MongoHelper.map(account)
   }
 }
