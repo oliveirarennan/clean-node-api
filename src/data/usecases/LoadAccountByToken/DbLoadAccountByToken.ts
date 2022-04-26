5b11a4adba91a17bd1d9ff0684e2c58b622b7965
Rennan Neves de Oliveira <rennan_neves@live.com>
2022-04-26 15:10:01 -0300

    feat: ensure DbLoadAccountByToken return an account on success

--- src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.ts
+++ src/data/usecases/LoadAccountByToken/DbLoadAccountByToken.ts
@@ -13,8 +13,12 @@ export class DbLoadAccountByToken implements LoadAccountByToken {
     const token = await this.decrypter.decrypt(accessToken)
 
     if (token) {
-      await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+      const account = await this.loadAccountByTokenRepository.loadByToken(accessToken, role)
+
+      if (account) {
+        return account
+      }
     }
-    return await new Promise(resolve => resolve(null))
+    return null
   }
 }
